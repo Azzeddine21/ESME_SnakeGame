1f61e9df7afefb451eab0bf63de4245c3cf3ab0c
CCabricot <81744905+CCabricot@users.noreply.github.com>
2022-04-03 10:47:32 +0200

    mineur

--- scripts/choix_jeu.js
+++ scripts/choix_jeu.js
@@ -1,53 +1,35 @@
+const item0 = document.getElementById("item-0");
+const item1 = document.getElementById("item-1");
+const item2 = document.getElementById("item-2");
+const item3 = document.getElementById("item-3");
+const item4 = document.getElementById("item-4");
+const imgList = [item0.src,item1.src,item2.src,item3.src,item4.src];
 
+function left(){
+    imgList.unshift(imgList.pop());
+    update()
+}
 
-function droite(){
-    var item1 = document.getElementById("item-1");
-    var item2 = document.getElementById("item-2");
-    var item3 = document.getElementById("item-3");
-    var item4 = document.getElementById("item-4");
-    var item5 = document.getElementById("item-5");
+function right(){
 
-    var imgList = [item1.src,item2.src,item3.src,item4.src,item5.src];
-    
-    for(i = 1; i<6;i++){
-        var index = (i-2)%5;
-        if(index<0){
-            index=5+index;
-        }
-        item = document.getElementById(`item-${i}`);
-        console.log(`item-${i}`);
-        item.src = imgList[index];
-        console.log(index);
-        
-    }
+    imgList.push(imgList.shift());
+    update()
 
 }
 
-function gauche(){
-    var item1 = document.getElementById("item-1");
-    var item2 = document.getElementById("item-2");
-    var item3 = document.getElementById("item-3");
-    var item4 = document.getElementById("item-4");
-    var item5 = document.getElementById("item-5");
-
-    var imgList = [item1.src,item2.src,item3.src,item4.src,item5.src];
-    
-    var container_jeu=document.getElementById("container_jeu");
-
-    for(i = 1; i<6;i++){
+function update(){
+    for(i = 0; i<5;i++){
         var index = i%5;
-
         item = document.getElementById(`item-${i}`);
         item.src = imgList[index];
         
     }
 }
-function initialisation(){
-    var button_droite = document.getElementById("droite"); 
-    button_droite.addEventListener("click", droite);
 
-    var button_gauche = document.getElementById("gauche"); 
-    button_gauche.addEventListener("click", gauche);
+function initialize(){
+    var buttonLeft = document.getElementById("left");
+    var buttonRight = document.getElementById("right");
+
+    buttonLeft.innerText("<Previous");
+    buttonRight.innerText("Next>");
 }
-
-initialisation();
